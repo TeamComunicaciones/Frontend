e78357c2e3ff7721454498008e46605d47a0f41c
Mirko <moonsethra@gmail.com>
2025-08-06 16:18:02 -0500

    Cambio de precios 2

--- src/components/BackendRouter/BackendRouter.js
+++ src/components/BackendRouter/BackendRouter.js
@@ -1,6 +1,6 @@
 
 var backendRoute = {
-    data : 'https://api.teamcomunicaciones.com.co/api/v1.0/'
+    data : 'https://api.teamcomunicaciones.com.co/api/v1.0//api/v1.0/get_filtros_precios/'
     //data : 'http://localhost:8000/api/v1.0/'
 }
 
